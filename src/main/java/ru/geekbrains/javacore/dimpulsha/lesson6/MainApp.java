90fc9ca41da69aabf0d49dd7ace099a63d229e25
dimpulsha <dimpulsha@gmail.com>
2021-07-15 19:17:45 +0500

    Lesson6 HomeWork - удален лишний tst.html

--- src/main/java/ru/geekbrains/javacore/dimpulsha/lesson6/MainApp.java
+++ src/main/java/ru/geekbrains/javacore/dimpulsha/lesson6/MainApp.java
@@ -10,18 +10,18 @@ public class MainApp {
 
         Socket socket = new Socket("www.nyu.edu", 80);
 
-        InputStream response = socket.getInputStream();
-        OutputStream request = socket.getOutputStream();
-
-        byte[] bytes = ("GET /community.html HTTP/1.0\n" +
-                "Host: www.nyu.edu\n\n").getBytes();
-
-        request.write(bytes);
-
-        int ch;
-        while ((ch = response.read()) != -1) {
-            System.out.print((char) ch);
-        }
+//        InputStream response = socket.getInputStream();
+//        OutputStream request = socket.getOutputStream();
+//
+//        byte[] bytes = ("GET /community.html HTTP/1.0\n" +
+//                "Host: www.nyu.edu\n\n").getBytes();
+//
+//        request.write(bytes);
+//
+//        int ch;
+//        while ((ch = response.read()) != -1) {
+//            System.out.print((char) ch);
+//        }
         socket.close();
     }
 }
